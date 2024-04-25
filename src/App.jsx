1ece61a1e009edaea1880c5d7a430af9d8beda13
arunvenkat <73863663+sjarunvenkat@users.noreply.github.com>
2024-04-25 09:58:05 +0000

    updated solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-import './App.css';
+import React from 'react'
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
   // code here
+  return (
+    <>
+    <h1 className='heading'>Kalvium Gallery</h1>
+    <div className="App">
+    {imageData().map((img) => (
+      <div key={img.id}>
+        <img src={img.img} alt="elephant" />
+      </div>
+    ))}
+  </div>
+  </>
+    
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,43 @@
-import { Component } from "react";
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
   // code here
+  render(){
+    return(
+      <>
+      <h1 className="heading">Kalvium Gallery</h1>
+      <div className="App">
+        {this.imageData().map((img)=>(
+          <div key={img.id}>
+            <img src={img.img} alt="elephant" />
+          </div>
+        ))}
+      </div>
+      </>
+    )
+  }
 }
